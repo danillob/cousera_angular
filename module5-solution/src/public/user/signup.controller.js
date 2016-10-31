94544cdece748ebf9b6cb0b7bc342174f8cfc87b
danillob <danillob@gmail.com>
2016-10-31 16:29:43 -0200

    Module 5 initial submit

---
+++ module5-solution/src/public/user/signup.controller.js
@@ -0,0 +1,17 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+function SignUpController() {
+  var signUpCtrl = this;
+
+  signUpCtrl.submit = function () {
+    console.log('foi!');
+  };
+
+}
+
+
+})();
